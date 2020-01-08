9ebc7829c6bb8923f5fa4e75fa9d83dd88a7965c
andemarco <andreademarco91@gmail.com>
2020-01-08 16:48:53 +0100

    Lista funzionante

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-alert('ciao')
+// LISTA COGNOMI
+var cognomi = ['zorro', 'lelardo', 'sagnale', 'datome', 'fige', 'verro', 'gagnale'];
+// CHIEDERE COGNOME
+var cognomeInserito = prompt('Inserisci qui il tuo cognome');
+// INSERIRE COGNOME IN ARREY
+cognomi.push(cognomeInserito);
+// CREARE LISTA DI COGNOMI IN ORDINE ALFABETICO
+cognomi.sort();
+console.log(cognomi);
+// STAMPARE LISTA
+for (var i = 0; i < cognomi.length; i++) {
+  var cognomiOk = document.getElementById('lista_cognomi').innerHTML;
+  document.getElementById('lista_cognomi').innerHTML =  cognomiOk + '<li>'+ " " + cognomi[i] + '</li>'
+}
